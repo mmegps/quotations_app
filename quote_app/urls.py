cf8f80cff3ab4906ca3b7131577777228a56bd1d
mmegps <mmegps@gmail.com>
2017-02-12 20:53:43 +0000

    Added first render and an html template.

--- quote_app/urls.py
+++ quote_app/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from quotations import views
+
 urlpatterns = [
+    url(regex=r'^$',
+        view=views.index,
+        name='home'
+    ),
     url(r'^admin/', admin.site.urls),
 ]
