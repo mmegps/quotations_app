dde957f3af7f7c47361f53e20459ab63248616eb
mmegps <mmegps@gmail.com>
2017-02-12 19:37:22 +0000

    Initial commit.

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mohitmehta/Desktop/MyPersonal/python_experiments/quotations_app/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
