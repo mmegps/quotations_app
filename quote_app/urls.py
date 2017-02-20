d912f68b505e2a131bf5641ca81c16822ff43cf4
mmegps <mmegps@gmail.com>
2017-02-20 20:03:10 +0000

    Added css and other templates.

--- quote_app/urls.py
+++ quote_app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from quotations import views
 
@@ -23,5 +24,11 @@ urlpatterns = [
         view=views.index,
         name='home'
     ),
+    url(r'^about',
+    	TemplateView.as_view(template_name='about.html'),
+    	name='about'),
+    url(r'^contact', 
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
