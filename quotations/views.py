a172e393e9b2ee8af8a7120df1c792b9f9631e63
mmegps <mmegps@gmail.com>
2017-02-27 09:18:29 +0000

    Added support for slugs on quotations.

--- quotations/views.py
+++ quotations/views.py
@@ -4,11 +4,17 @@ from quotations.models import Qs
 # Create your views here.
 def index(request):
 	count = Qs.objects.count()
-
-	#things = Qs.objects.all()
-	things =  Qs.objects.all().filter(quote__icontains="the")
+	things = Qs.objects.all()
 
 	return render(request, 'index.html',
 	             {'total_count': count,
 	             'things' : things,
 	             })
+
+# new view
+def quote_detail(request, slug):
+	quote = Qs.objects.get(slug=slug)
+
+	return render(request, 'quotes/quote_detail.html', {
+		'quote': quote,
+		})
